a8c6853ebd1030ffd04015153a9cb737f74d6983
Sanndcn <sandydcn29@gmail.com>
2026-06-07 21:22:55 +0700

    Ubah warna input text

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -201,7 +201,7 @@ export function Input({ label, name, type = 'text', value, onChange, placeholder
         placeholder={placeholder}
         required={required}
         step={step}
-        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-shadow"
+        className="w-full px-3 py-2 border border-gray-300 rounded-lg text-black focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-shadow"
       />
     </div>
   );
