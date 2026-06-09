234a19af1ceaa9c086d56450b7477646242511e0
Sanndcn <sandydcn29@gmail.com>
2026-06-09 17:03:07 +0700

    Fixing bugs

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,8 @@ export default function LoginPage() {
       });
 
       if (res.ok) {
-        router.push('/');
+        window.location.href = '/';
+        
       } else {
         const data = await res.json();
         setError(data.error || 'Password salah');
